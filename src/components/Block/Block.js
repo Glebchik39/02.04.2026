6272eef031c35e5304b5e62b6c7e6473cdfbcc03
hello <hell@gmail.com>
2026-04-02 12:26:03 +0300

    рапарпрарпапрарпрпрп

---
+++ src/components/Block/Block.js
@@ -0,0 +1,14 @@
+import './Block.css';
+
+export function Block()
+{
+   return <div >
+      <center><h1>Тема 1</h1></center>
+      <div className='content'>
+      <a src="hhtp:pornhub">Теорія</a>
+      <a src ="">Тести</a>  
+      </div>   
+   </div>
+}
+
+export default Block;
---
+++ src/components/Teory/Teory.js
@@ -0,0 +1,15 @@
+import './Teory.css';
+
+
+
+export function Teory() 
+{
+   return <div>
+      <p>vbnbcjbkcjvkbj</p>
+      <button><a src="">Tести</a></button>
+      <a src="">Назад</a>
+   </div>
+   
+}
+
+export default Teory;
